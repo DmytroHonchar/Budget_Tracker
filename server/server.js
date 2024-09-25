40f30d6f1405916219a45b73b61ba1d9fe1da982
DmytroHonchar <126788840+DmytroHonchar@users.noreply.github.com>
2024-09-25 11:09:13 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -65,7 +65,11 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false, // Accept self-signed certificates
+    },
 });
+
 
 // Create a nodemailer transporter
 const transporter = nodemailer.createTransport({
